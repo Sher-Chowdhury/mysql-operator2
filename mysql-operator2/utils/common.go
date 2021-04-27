6f77c8bc2ddcdf016d99777f1260237b105b0df3
Sher Chowdhury <sher.chowdhury@ibm.com>
2021-04-27 12:10:57 +0100

    testing

---
+++ mysql-operator2/utils/common.go
@@ -0,0 +1,14 @@
+package utils
+
+import (
+	"github.com/go-logr/logr"
+	"k8s.io/apimachinery/pkg/runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+// MysqlReconciler reconciles a Mysql object
+type MysqlReconciler struct {
+	client.Client
+	Log    logr.Logger
+	Scheme *runtime.Scheme
+}
